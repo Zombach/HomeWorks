649dda203f113d14483bc0308e3a58206f6bfe3a
ZLoo <52016832+Zombach@users.noreply.github.com>
2021-03-11 13:44:43 +0300

    Delete MainMenu.Tests/bin/Debug/net5.0 directory

--- MainMenu/Quest/Variables .cs
+++ MainMenu/Quest/Variables .cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MainMenu.Text;
+using MainMenu.Quest.VariablesFolders;
 
 namespace MainMenu.Quest
 {
@@ -11,72 +12,23 @@ namespace MainMenu.Quest
             switch (programClass.dataTextY)
             {
                 case 1:
-                    tmp = Quest_1(programClass);
+                    tmp = VariablesQ_1.Quest(programClass);
                     break;
                 case 2:
-                    tmp = Quest_2(programClass);
+                    tmp = VariablesQ_2.Quest(programClass);
                     break;
                 case 3:
-                    tmp = Quest_3(programClass);
+                    tmp = VariablesQ_3.Quest(programClass);
                     break;
                 case 4:
-                    tmp = Quest_4(programClass);
+                    tmp = VariablesQ_4.Quest(programClass);
                     break;
                 case 5:
-                    tmp = Quest_5(programClass);
+                    tmp = VariablesQ_5.Quest(programClass);
                     break;
                 default:
                     break;
             }
-            return tmp;
-        }
-
-        private static double[] Quest_1(ProgramClass programClass)
-        {
-            double[] tmp = new double[1];
-            tmp[0] = (5 * programClass.dataQueriesClass.number_1 + programClass.dataQueriesClass.number_2 * programClass.dataQueriesClass.number_2) /
-                (programClass.dataQueriesClass.number_2 - programClass.dataQueriesClass.number_1);
-            return tmp;
-        }
-
-        private static double[] Quest_2(ProgramClass programClass)
-        {
-            double[] tmp = new double[2];
-            tmp[0] = programClass.dataQueriesClass.number_1;
-            programClass.dataQueriesClass.number_1 = programClass.dataQueriesClass.number_2;
-            programClass.dataQueriesClass.number_2 = (int)tmp[0];
-            tmp[0] = programClass.dataQueriesClass.number_1;
-            tmp[1] = programClass.dataQueriesClass.number_2;
-            return tmp;
-        }
-        private static double[] Quest_3(ProgramClass programClass)
-        {
-            double[] tmp = new double[2];
-            tmp[0] = programClass.dataQueriesClass.number_1 / programClass.dataQueriesClass.number_2;
-            tmp[1] = programClass.dataQueriesClass.number_1 % programClass.dataQueriesClass.number_2;
-            return tmp;
-        }
-        private static double[] Quest_4(ProgramClass programClass)
-        {
-            double[] tmp = new double[1];
-            if (programClass.dataQueriesClass.number_1 == 0)
-            {
-                if (programClass.dataQueriesClass.number_2 == programClass.dataQueriesClass.number_3)
-                {
-                    programClass.dataTextClass.answerString++;
-                }
-                programClass.dataTextClass.answerString++;
-                tmp = null;
-                return tmp;
-            }
-            tmp[0] = (double) (programClass.dataQueriesClass.number_3 - programClass.dataQueriesClass.number_2) / programClass.dataQueriesClass.number_1;
-            return tmp;
-        }
-        private static double[] Quest_5(ProgramClass programClass)
-        {
-            double[] tmp = new double[2];
-            tmp[0] = (programClass.dataQueriesClass.number_2 - programClass.dataQueriesClass.number_4) / (programClass.dataQueriesClass.number_1 - programClass.dataQueriesClass.number_3);
-            tmp[1] = programClass.dataQueriesClass.number_4 - tmp[0] * programClass.dataQueriesClass.number_2;
             return tmp;
         }
     }
---
+++ MainMenu/Quest/VariablesFolders/VariablesQ_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MainMenu.Text;
+
+namespace MainMenu.Quest.VariablesFolders
+{
+    public static class VariablesQ_4
+    {
+        public static double[] Quest(ProgramClass programClass)
+        {
+            double[] tmp = new double[1];
+            if (programClass.dataQueriesClass.number_1 == 0)
+            {
+                if (programClass.dataQueriesClass.number_2 == programClass.dataQueriesClass.number_3)
+                {
+                    programClass.dataTextClass.answerString++;
+                }
+                programClass.dataTextClass.answerString++;
+                tmp = null;
+                return tmp;
+            }
+            tmp[0] = (double)(programClass.dataQueriesClass.number_3 - programClass.dataQueriesClass.number_2) / programClass.dataQueriesClass.number_1;
+            return tmp;
+        }
+    }
+}
